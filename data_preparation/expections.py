f44cc1ebffaa54a3a205e6c35fe6185f9300e170
Tamino <tw@digio.ch>
2025-01-04 20:48:10 +0100

    added more and better data and improved clustering

---
+++ data_preparation/expections.py
@@ -0,0 +1,102 @@
+test_set = {
+    '1uvyZBs4IZYRebHIB1747m': 1,  # Purple Rain : funk rock
+    '3S2R0EVwBSAVMd5UMgKTL0': 5,  # Thriller : soul r&b
+    '2374M0fQpWi3dLnB54qaLX': 1,  # Africa : soft rock
+    '4RvWPyQ5RL0ao9LPZeSouE': 2,  # Everybody Wants To Rule The World : new wave pop
+    '2MuWTIM3b0YEAskbeeFE1i': 4,  # Master Of Puppets : thrash metal
+    '2fuCquhmrzHpu5xcA1ci9x': 1,  # Under Pressure : classic rock
+    '2WfaOiMkCvy7F5fcp2zZ8L': 2,  # Take on Me : new wave pop
+    '37ZJ0p5Jm13JPevGcx4SkF': 1,  # Livin' On A Prayer : rock
+    '77NNZQSqzLNqh2A9JhLRkg': 1,  # Don't Stop Believin' : soft rock
+    '4N0fzRX3T7QkOecp3pkWpp': 1,  # With Or Without You : rock
+    '4CeeEOM32jQcH3eN9Q2dGj': 1,  # Smells Like Teen Spirit : rock
+    '6MdqqkQ8sSC0WB4i8PyRuQ': 6,  # No Diggity : gangster rap
+    '4r8lRYnoOGdEi6YyI5OC1o': 3,  # Bye Bye Bye : dance pop
+    '57bgtoPSgt236HzfBOd8kj': 1,  # Thunderstruck : rock
+    '47BBI51FKFwOMlIiX6m8ya': 3,  # I Want It That Way : dance pop
+    '0hrBpAOgrt8RXigk83LLNE': 2,  # The Sign : new wave pop
+    '1Jaah2tmN9Hv81A87KZ1MU': 2,  # Torn : new wave dance pop
+    '1qPbGZqppFwLwcBC1JQ6Vr': 1,  # Wonderwall : rock
+    '1WYEDcS7WGjv0rG7rmMX3o': 1,  # Bitter Sweet Symphony : rock
+    '7H6ev70Weq6DdpZyyTmUXk': 3,  # Say My Name : r&b dance pop
+    '1QV6tiMFM6fSOKOGLMHYYg': 3,  # Poker Face : dance pop
+    '3DQVgcqaP3iSMbaKsd57l5': 1,  # I Bet You Look Good On The Dancefloor : rock
+    '5VGlqQANWDKJFl0MBG3sg2': 1,  # Use Somebody : rock
+    '0KQx6HOpJueiSkztcS0r7D': 5,  # Fallin' : r&b pop soul
+    '3TCauNPqFiniaYHBvEVoHG': 3,  # Beautiful : dance pop
+    '7IdFdRlCjUi6kkhbPoRfnw': 6,  # 99 Problems : gangster rap
+    '1vxw6aYJls2oq3gW0DujAo': 5,  # Crazy : soul hip hop
+    '1ixbwbeBi5ufN4noUKmW5a': 8,  # Paper Planes : new rave
+    '3dPQuX8Gs42Y7b454ybpMR': 1,  # Seven Nation Army : alternative rock blues
+    '1L5tZi0izXsi5Kk5OJf4W0': 5,  # Rehab : new soul
+    '4OSBTYWVwsQhGLF9NHvIbR': 2,  # Rolling in the Deep : pop soul
+    '3iVcZ5G6tvkXZkZKlMpIUs': 7,  # Alright : hip hop
+    '3DK6m7It6Pw857FcQftMds': 6,  # Runaway : trap gangster rap
+    '3e9HZxeyfWwjeyPAMmWSSQ': 2,  # thank u, next : pop
+    '58q2HKrzhC3ozto2nDdN4z': 7,  # I Like It : latino rap
+    '4SDWMFvYJKtK1QPhIOeZhd': 2,  # Royals : alternative pop
+    '0wwPcA6wtMf6HUMpIRdeP7': 7,  # Hotline Bling : hip hop
+    '0F7FA14euOIX8KcbEturGH': 7,  # Old Town Road : country rock
+    '2VxeLyX666F8uXCJ0dZF8B': 3,  # Shallow : dance pop
+    '2Foc5Q5nqNiosCNqttzHof': 3,  # Get Lucky : disco dance pop
+    '3z8h0TU7ReDPLIbEnYhWZb': 1,  # Bohemian Rhapsody : rock
+    '4UDmDIqJIbrW0hMBQMFOsM': 1,  # Stayin' Alive : soft rock
+    '5CQ30WqJwcep0pYcV4AMNc': 1,  # Stairway to Heaven : rock
+    '0GjEhVFGZW8afUYGChu3Rr': 2,  # Dancing Queen : pop
+    '0ofHAoxe9vBkTCp2UQIavz': 1,  # Dreams : soft rock
+    '4xHWH1jwV5j4mBYRhxPbwZ': 1,  # Time : symphonic psychedelic rock
+    '40riOy7x9W7GXjyGp4pjAv': 1,  # Hotel California : soft rock
+    '4N0TP4Rmj6QQezWV88ARNJ': 5,  # Superstition : soul
+    '2grjqo0Frpf2okIBiifQKs': 5,  # September : disco soul funk
+    '2SpEHTbUuebeLkgs9QB7Ue': 2,  # Jolene : country pop
+    '2IJftBfq7pJ43tfnOR0RB3': 2,  # 99 Luftballons : new wave pop
+    '0DfG1ltJnZyq4Tx3ZLL7ZU': 2,  # Rock Me Amadeus : new wave
+    '5vZ4IeUenK2cHub2d7yfWk': 4,  # Radio : industrial rock metal
+    '3NIDOSThELMihLSOMZcL4k': 7,  # Hurra die Welt geht unter : hip hop
+    '5LH1z4ma2TN2aVeESXthj9': 7,  # Ding : hip hop house alternative dance
+    '228BxWXUYQPJrJYHDLOHkj': 8,  # Gasolina : latino trap hip hop
+    '6mICuAdrwEjh6Y6lroV2Kg': 8,  # Chantaje : latin trap dance pop
+    '5w9c2J52mkdntKOmRLeM2m': 8,  # Con Calma : latin trap hip hop
+    '4sCSUQnWQ6HJsOMjfPicdn': 8,  # Danza Kuduro : latino trap hip hop
+    '3i3fYhl23l7exbIj9oAJPN': 2,  # Ahora Quien : salsa latin pop
+    '1MqzIgGsHP4cNjPboevsfq': 2,  # Tourner Dans Le Vide : pop
+    '34dx8DACTJsc3rsJdaEIQw': 2,  # Papaoutai : house pop
+    '6RU0eIObzPf9HxYYV2hh5G': 2,  # Toi jamais : pop chanson
+    '2d8D7uk3tbAThjRkdfrx9c': 2,  # Voyage voyage : disco synth pop
+    '6IRA4KOVbtiGiTdYoEThJN': 9,  # Ne me quitte pas : chanson singer-songwriter
+    '7sXTcibuNYugjjYNfgf2kY': 2,  # Felicità : pop
+    '2S7RApTsKT0CtYojYq2cKz': 2,  # L'italiano : pop
+    '776AftMmFFAWUIEAb3lHhw': 1,  # ZITTI E BUONI : pop rock
+    '5zyrEv4F3FaLECI8TOKpFM': 2,  # Nel Blu Dipinto Di Blu : pop
+    '0jLeyuu6mtsD384054xLah': 2,  # Ciao Ciao : alternative pop
+    '1vePC77pxGSUXLErDHuGtH': 6,  # The Message : old school hip hop
+    '5Tbpp3OLLClPJF8t1DmrFD': 6,  # Nuthin\' But A "G" Thang : gangster rap
+    '5n8Aro6j1bEGIy7Tpo7FV7': 6,  # Fuck Tha Police : gangster rap
+    '7MJQ9Nfxzh8LPZ9e9u68Fq': 7,  # Lose Yourself : hip hop
+    '7iL6o9tox1zgHpKUfh9vuC': 6,  # In Da Club : gangster rap
+    '0cGG2EouYCEEC3xfa0tDFV': 2,  # Tainted Love : synth new wave pop
+    '39lSeqnyjZJejRuaREfyLL': 2,  # Hungry Like the Wolf : synth new wave pop
+    '4OdhffpvG3HxDJMxYOKh97': 2,  # I Ran (So Far Away) : synth new wave pop
+    '0qi4b1l0eT3jpzeNHeFXDT': 2,  # Just Can't Get Enough : dance pop
+    '3L7RtEcu1Hw3OXrpnthngx': 2,  # Don't You Want Me : synth new wave pop
+    '7ACxUo21jtTHzy7ZEV56vU': 4,  # Crazy Train : rock metal
+    '78lgmZwycJ3nzsdgmPPGNx': 1,  # Immigrant Song : rock
+    '5sICkBXVmaCQk5aISGR3x1': 4,  # Enter Sandman : thrash metal
+    '3CIOopLwvyMvXk97ZEksKO': 4,  # Ace of Spades : rock metal
+    '54yLLvpezvPO1lA3GspiZj': 4,  # Holy Diver : rock metal
+    '0WtDGnWL2KrMCk0mI1Gpwz': 7,  # Redbone : hip hop
+    '0FZvjrHpAmLKj574M4VwrF': 3,  # Cater 2 U : r&b dance pop
+    '3NWTRZ0A8xKlBP1qgNftql': 2,  # Special Affair : alternative r&b
+    '6g0Orsxv6glTJCt4cHsRsQ': 2,  # Formation : r&b pop
+    '4pAl7FkDMNBsjykPXo91B3': 2,  # Needed Me : r&b pop
+    '2YWjW3wwQIBLNhxWKBQd16': 3,  # Lean On : electronic dance pop
+    '7BKLCZ1jbUBVqRi2FVlTVw': 3,  # Closer : electronic pop
+    '5knuzwU65gJK7IF5yJsuaW': 3,  # Rockabye : dance pop
+    '0TDLuuLlV54CkRRUOahJb4': 3,  # Titanium : dance pop edm house
+    '0nrRP2bk19rLc0orkWPQk2': 3,  # Wake Me Up : dance pop edm
+    '1hwJKpe0BPUsq6UUrwBWTw': 1,  # Boulevard of Broken Dreams : rock
+    '6md4w95cnKaW64JyEQ3A0L': 2,  # Somebody That I Used To Know : electronic pop
+    '60a0Rd6pjrkxjPbaKzXjfq': 4,  # In the End : rock metal
+    '6qa36OkEeQqGaWlOcpjlGD': 1,  # Sunday Bloody Sunday : rock pop
+    '3Fcfwhm8oRrBvBZ8KGhtea': 2,  # Viva La Vida : rock pop
+}
